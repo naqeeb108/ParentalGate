c0666892f7cec86d3ef9dc8fae6fb6f16dc9e3bc
naqeeb_ahmad <naqeeb@codesorbit.com>
2021-06-09 22:32:48 +0500

    added functions for success and error messages

---
+++ Sources/Controllers/ParentalGateQuestions.swift
@@ -0,0 +1,20 @@
+//
+//  ParentalGateQuestions.swift
+//  ParentalGate
+//
+//  Created by Mac on 09/06/2021.
+//
+
+import UIKit
+
+class ParentalGateQuestions: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
---
+++ Sources/Protocols/ParentalGateProtocols.swift
@@ -0,0 +1,12 @@
+//
+//  ParentalGateProtocols.swift
+//  ParentalGate
+//
+//  Created by Mac on 09/06/2021.
+//
+
+import Foundation
+protocol ParentalGateDelegate {
+    func success()
+    func fail()
+}
